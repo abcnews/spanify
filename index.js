932113dc89f871e604a2243713b0130b7b97799b
Joshua Byrd <phocks@gmail.com>
2017-12-15 13:27:14 +1000

    Add divify()

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 "use strict";
 
+// Scans DOM for <a title="whatever"> </a> some text <a title="end"> </a>
+// and converts to <span class="whatever">some text</span>
+
 exports.spanify = function spanify() {
   // Get an array of all the anchor elements on the page
   var anchors = document.querySelectorAll("a");
 
   // Loop through all the anchors (we need to use an array hack for IE/Edge support)
-  [].forEach.call(anchors, function(anchor) {
+  [].forEach.call(anchors, function (anchor) {
     // Leave normal links on the page alone
     if (anchor.innerHTML !== " ") return;
     // Leave #hashtag links alone
@@ -35,5 +38,32 @@ exports.spanify = function spanify() {
     anchor.parentNode.replaceChild(spanEl, anchor);
     // Remove the remaining in between text
     spanTextEl.parentNode.removeChild(spanTextEl);
+  });
+};
+
+// Scans DOM for <a name="whatever"> </a>
+// and converts to <div class="whatever"></div>
+exports.divify = function divify() {
+  // Get an array of all the anchor elements on the page
+  var anchors = document.querySelectorAll("a");
+
+  // Loop through all the anchors (we need to use an array hack for IE/Edge support)
+  [].forEach.call(anchors, function (anchor) {
+    // Leave normal links on the page alone
+    if (anchor.innerHTML !== " ") return;
+    // Leave #hashtag links alone
+    if (anchor.getAttribute("title")) return;
+    // Make sure it's really a #hashlink
+    if (!anchor.getAttribute("name")) return;
+
+    // The anchor title will later become the span class
+    var elementName = anchor.getAttribute("name");
+
+    // Compose our new div element
+    var divEl = document.createElement("div");
+    divEl.setAttribute("class", elementName);
+
+    // Replace anchor with div
+    anchor.parentNode.replaceChild(divEl, anchor);
   });
 };
