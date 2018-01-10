d0b07a49ceec0facc5dfbaac725d4fe1687ad5b0
Joshua Byrd <phocks@gmail.com>
2018-01-10 15:38:15 +1000

    Implement rollup

---
+++ main.js
@@ -0,0 +1,84 @@
+// Scans DOM for <a title="whatever"> </a> some text <a title="end"> </a>
+// and converts to <span class="whatever">some text</span>
+
+export function spanify(options) {
+  // Get an array of all the anchor elements on the page
+  const anchors = document.querySelectorAll("a");
+
+  // Loop through all the anchors (we need to use an array hack for IE/Edge support)
+  [].forEach.call(anchors, anchor => {
+    // Leave normal links on the page alone
+    if (anchor.innerHTML !== " ") return;
+    // Leave #hashtag links alone
+    if (!anchor.getAttribute("title")) return;
+
+    // The anchor title will later become the span class
+    const elementTitle = anchor.getAttribute("title");
+
+    // If it is an "end" tag it will already have made the span
+    if (elementTitle.slice(0, 3) === "end") {
+      // So we don't need it any more...
+      anchor.parentNode.removeChild(anchor);
+      return;
+    }
+
+    // Compose our span element
+    const spanEl = document.createElement("span");
+    spanEl.setAttribute("class", elementTitle);
+
+    // Add a default class if passed one
+    if (options && options.defaultClass) {
+      addClass(spanEl, options.defaultClass);
+    }
+
+    // Store the text in between the two anchor tags
+    const spanTextEl = anchor.nextSibling;
+    spanEl.innerHTML = spanTextEl.textContent.trim();
+
+    // To replace the anchor apparently the span needs to be appended
+    anchor.parentNode.appendChild(spanEl);
+
+    // Replace the first anchor tag
+    anchor.parentNode.replaceChild(spanEl, anchor);
+    // Remove the remaining in between text
+    spanTextEl.parentNode.removeChild(spanTextEl);
+  });
+}
+
+// Scans DOM for <a name="whatever"> </a>
+// and converts to <div class="whatever"></div>
+export function hashify(options) {
+  // Get an array of all the anchor elements on the page
+  const anchors = document.querySelectorAll("a");
+
+  // Loop through all the anchors (we need to use an array hack for IE/Edge support)
+  [].forEach.call(anchors, anchor => {
+    // Leave normal links on the page alone
+    if (anchor.innerHTML !== " ") return;
+    // Leave #hashtag links alone
+    if (anchor.getAttribute("title")) return;
+    // Make sure it's really a #hashlink
+    if (!anchor.getAttribute("name")) return;
+
+    // The anchor title will later become the span class
+    const elementName = anchor.getAttribute("name");
+
+    // Compose our new div element
+    const divEl = document.createElement("div");
+    divEl.setAttribute("class", elementName);
+
+    // Add a default class if passed one
+    if (options && options.defaultClass) {
+      addClass(divEl, options.defaultClass);
+    }
+
+    // Replace anchor with div
+    anchor.parentNode.replaceChild(divEl, anchor);
+  });
+}
+
+// Some convenience methods
+function addClass(el, className) {
+  if (el.classList) el.classList.add(className);
+  else if (!hasClass(el, className)) el.className += ` ${className}`;
+}
---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import babel from "rollup-plugin-babel";
+import uglify from "rollup-plugin-uglify";
+
+export default {
+  input: "main.js",
+  output: {
+    file: "index.js",
+    format: "umd",
+    name: "spanify"
+  },
+  plugins: [
+    babel({
+      exclude: 'node_modules/**' // only transpile our source code
+    }),
+    uglify()
+  ]
+}
